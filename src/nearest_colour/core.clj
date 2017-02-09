d396bf7260e13b2103bdc0f1ef84d72a0db9e703
James Oldfield <hi@james-oldfield.co>
2017-02-09 15:38:29 +0000

    implement 3d euclid distance func

--- src/nearest_colour/core.clj
+++ src/nearest_colour/core.clj
@@ -1,6 +1,13 @@
 (ns nearest-colour.core
+  (:require [clojure.math.numeric-tower :as math])
   (:gen-class))
 
+(defn compute_3d_dist
+  [[x1 y1 z1]
+   [x2 y2 z2]]
+  (math/sqrt (+ (math/expt (- x1 x2) 2)
+                (math/expt (- y1 y2) 2)
+                (math/expt (- z1 z2) 2))))
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
