574d047481f67188184ae6b3c36c35f8ccf18d2a
James Oldfield <hi@james-oldfield.co>
2017-02-09 08:41:29 +0000

    move tests into respective util files

--- test/nearest_colour/convert_test.clj
+++ test/nearest_colour/convert_test.clj
@@ -1,16 +1,6 @@
 (ns nearest-colour.convert-test
   (:require [clojure.test :refer :all]
-            [nearest-colour.convert :refer :all]
-            [nearest-colour.valid_colour :refer :all]))
-
-(deftest test-is_colour_valid?
-  (testing "Is the colour input vec valid?"
-    (is (= true (is_colour_valid? [0 255 100])))
-    (is (= false (is_colour_valid? 2 24 0)))
-    (is (= false (is_colour_valid? "2 24 0")))
-    (is (= false (is_colour_valid? "bob")))
-    (is (= false (is_colour_valid? [0 255 -100])))
-    (is (= false (is_colour_valid? [0])))))
+            [nearest-colour.convert :refer :all]))
 
 (deftest test-rgb_to_XYZ
   (testing "RGB -> XYZ conversion"
---
+++ test/nearest_colour/valid_colour_test.clj
@@ -0,0 +1,12 @@
+(ns nearest-colour.convert-test
+  (:require [clojure.test :refer :all]
+            [nearest-colour.valid_colour :refer :all]))
+
+(deftest test-is_colour_valid?
+  (testing "Is the colour input vec valid?"
+    (is (= true (is_colour_valid? [0 255 100])))
+    (is (= false (is_colour_valid? 2 24 0)))
+    (is (= false (is_colour_valid? "2 24 0")))
+    (is (= false (is_colour_valid? "bob")))
+    (is (= false (is_colour_valid? [0 255 -100])))
+    (is (= false (is_colour_valid? [0])))))
