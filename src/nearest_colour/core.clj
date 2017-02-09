8dc8197145a0c494559118a124bd84f452127dfd
James Oldfield <hi@james-oldfield.co>
2017-02-09 16:33:49 +0000

    return list of euclid distances between input and colour in CIE-LAB-space

--- src/nearest_colour/core.clj
+++ src/nearest_colour/core.clj
@@ -1,15 +1,28 @@
 (ns nearest-colour.core
   (:require [clojure.math.numeric-tower :as math])
+  (:require [nearest-colour.convert :as convert])
   (:gen-class))
 
 (defn compute_3d_dist
-  "Computes the euclidean distance of two points in 3d space"
+  "Compute the euclidean distance of two points in 3d space"
   [[x1 y1 z1]
    [x2 y2 z2]]
   (math/sqrt (+ (math/expt (- x1 x2) 2)
                 (math/expt (- y1 y2) 2)
                 (math/expt (- z1 z2) 2))))
+
+; temp inputs, etc.
+(def palette [[196 34 74]
+              [98 231 42]
+              [124 195 204]
+              [0 0 13]
+              [24 199 0]
+              [255 0 255]])
+
+(def colour [255 99 71])
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Find most perceptually-similar colour in a palette to an input colour"
   [& args]
-  (println "Hello, World!"))
+    (map (partial compute_3d_dist (convert/RGB_to_LAB colour))
+         (map convert/RGB_to_LAB palette)))
